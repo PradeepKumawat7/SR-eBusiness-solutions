b7f35913111257a2056836a61a8aee2e1d4ab671
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-12-23 00:09:02 +0530

    Manifest.json added, idk why, I just thought

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
+<svelte:head>
+  <link rel="manifest" href="/manifest.json" />
+</svelte:head>
 <div class=" text-center bg-slate-500">
   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis laboriosam totam cum quisquam est quod voluptas, eum minima dignissimos harum. Soluta nisi at molestiae id debitis, consequatur voluptatibus libero ipsum?
 </div>
