2c3a0577d775db60052dad8e84da3fe7b61f66f4
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2024-01-07 16:38:18 +0530

    idk

---
+++ src/lib/footer.svelte
@@ -0,0 +1,7 @@
+<div>
+	<p class="text-sm font-normal text-center">© 2022. All rights reserved.</p>
+	<p class="text-sm font-normal text-center">Powered by <a href="https://svelte.dev/">Svelte</a></p>
+	<p class="text-sm font-normal text-center">
+		Built with <a href="https://kit.svelte.dev/">SvelteKit</a>
+	</p>
+</div>
