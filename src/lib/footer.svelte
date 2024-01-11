eae6ba7b8cdfeb960e88fe57eac94cb0c599fd98
BhJaipa <105307427+BhJaipal@users.noreply.github.com>
2024-01-11 13:14:17 +0530

    Other pages work start

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,7 +1,36 @@
-<div>
-	<p class="text-sm font-normal text-center">© 2022. All rights reserved.</p>
-	<p class="text-sm font-normal text-center">Powered by <a href="https://svelte.dev/">Svelte</a></p>
-	<p class="text-sm font-normal text-center">
-		Built with <a href="https://kit.svelte.dev/">SvelteKit</a>
-	</p>
-</div>
+<script>
+	import { services } from '$lib';
+	let serviceList = Object.keys(services);
+</script>
+
+<footer class="bg-gray-200 text-black absolute bottom-0 w-[100%]">
+	<div class="container mx-auto px-4">
+		<div class="h-16 block w-full text-center text-2xl font-extrabold">CompanyName</div>
+		<div class="grid grid-cols-2">
+			<div>
+				<div class="font-bold text-xl mb-1">Name</div>
+				<ul>
+					{#each serviceList as service}
+						<li class="text-base hover:font-semibold">
+							{service}
+						</li>
+					{/each}
+				</ul>
+			</div>
+			<div>
+				<div class="font-bold text-2xl mb-1">About</div>
+				<ul>
+					<li class="text-base hover:font-semibold">About Us</li>
+					<li class="text-base hover:font-semibold">Our Team</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+
+<style>
+	footer {
+		box-shadow: 0 -5px 100px 0 rgba(0, 0, 0, 0.2);
+		@apply text-center;
+	}
+</style>
---
+++ src/routes/contact-us/+page.svelte
@@ -0,0 +1,39 @@
+<div class="text-center">
+	<h1 class="text-3xl font-bold">Contact Us</h1>
+	<div class="flex text-center lg:flex-row xs:flex-col">
+		<div class="align-middle w-[45%]">
+			<div
+				class="form-box border-4 border-b-gray-400 border-r-gray-400 border-t-blue-400 border-l-blue-400 px-10 pt-10 text-lg h-[40vh] mx-[10%]"
+			>
+				<form>
+					<div class="flex flex-row">
+						<div class="grid grid-cols-2">
+							<label for="name">Name: </label><br />
+							<input type="text" name="name" id="name" />
+						</div>
+						<div class="grid grid-cols-2">
+							<label for="company-name">Company Name: </label><br />
+							<input type="text" name="company-name" id="company-name" />
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+		<div>info</div>
+	</div>
+</div>
+
+<style>
+	input {
+		@apply w-60 h-10 border-gray-500 border-2;
+	}
+	input:focus {
+		@apply w-60 h-10 border-gray-500 border-0;
+	}
+	label {
+		@apply text-lg w-auto text-left;
+	}
+	.form-box {
+		box-shadow: 10px 10px 20px rgba(30, 30, 30, 0.2);
+	}
+</style>
